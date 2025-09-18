03acc6c62541fd6cfa724a7346be45690da11ffa
paulus <paulusojak@gmail.com>
2025-09-18 22:22:27 +0700

    Add video thumbnail upload capability

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"io"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +33,61 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	
+	const maxMemory = 10 << 20
+	r.ParseMultipartForm(maxMemory)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	//get the image from HTML form and make sure it matched "thumbnail" input name
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	mediaType := header.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "missing Content-Type", nil)
+		return
+	}
+
+	//read all image data into a byte slice
+	data, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to read file", err)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "unable to get video data", err)
+		return
+	}
+
+	//check if the authenticated user is the video owner
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "video access unauthorized", nil)
+		return
+	}
+
+	newThumbnail := thumbnail {
+		data: data,
+		mediaType: mediaType,
+	}
+
+	// add thumbnail to the global map
+	videoThumbnails[video.ID] = newThumbnail
+
+	//update video metadata to the new thumbnail URL
+	thumbnailURL := fmt.Sprintf("http://localhost:%v/api/thumbnails/%v", cfg.port, video.ID)
+	video.ThumbnailURL = &thumbnailURL
+
+	//update the video record in the database
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "error updating video records", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
